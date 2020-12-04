4fffd4e8e4ba35b7e97506819ac86bcf6449041f
Jason9789 <lifebook0809@gmail.com>
2020-12-04 21:57:08 +0900

    [ADD][CLONE] Infinity Loader

---
+++ Infinity Loader/Infinity Loader/InfinityShape.swift
@@ -0,0 +1,30 @@
+//
+//  InfinityShape.swift
+//  Infinity Loader
+//
+//  Created by 전판근 on 2020/12/04.
+//
+
+import SwiftUI
+
+struct InfinityShape: Shape {
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.move(to: CGPoint(x: rect.midX - 100, y: rect.midY + 72))
+        
+        path.addCurve(to: CGPoint(x: rect.midX - 100, y: rect.midY - 72), control1: CGPoint(x: rect.midX - 200, y: rect.midY + 72), control2: CGPoint(x: rect.midX - 200, y: rect.midY - 72))
+        path.addCurve(to: CGPoint(x: rect.midX + 100, y: rect.midY + 72), control1: CGPoint(x: rect.midX , y: rect.midY - 72), control2: CGPoint(x: rect.midX, y: rect.midY + 72))
+        path.addCurve(to: CGPoint(x: rect.midX + 100, y: rect.midY - 72), control1: CGPoint(x: rect.midX + 200, y: rect.midY + 72), control2: CGPoint(x: rect.midX + 200, y: rect.midY - 72))
+        
+        path.addCurve(to: CGPoint(x: rect.midX - 100, y: rect.midY + 72), control1: CGPoint(x: rect.midX, y: rect.midY - 72), control2: CGPoint(x: rect.midX , y: rect.midY + 72))
+        return path
+    }
+}
+
+struct InfinityShaep_Previews: PreviewProvider {
+    static var previews: some View {
+        InfinityShape()
+            .stroke(style: StrokeStyle(lineWidth: 20, lineCap: .round))
+    }
+}
